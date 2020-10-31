ac35277d2cc84f67476b37156f3361effb114a39
ayesumit <sumitburmanelite@gmail.com>
2020-10-31 22:46:32 +0530

    Noth

---
+++ sound.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame import mixer
+mixer.init()
+soundExt = '.mp3'
+x = true
+
+pygame.mixer.Sound('assets/music/LoseControl' + soundExt)
+pygame.mixer.Sound('assets/music/LoseControl' + soundExt).play() 
+while x = true:
+	input('')
